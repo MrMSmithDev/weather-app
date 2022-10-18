02746292fe00edd442eeae61fe876f7da9300ad4
mbright90 <michael.bright90@icloud.com>
2022-10-18 14:16:37 +0100

    Add event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -3,4 +3,23 @@ import api from './utilities/apiManager'
 import dom from './utilities/domManager'
 
 dom.initHome()
-api.makeLocationSearch('Bs234xz')
+
+const navSearchBar = document.querySelector('.nav-location-input')
+const navSearchButton = document.querySelector('nav .search-button')
+navSearchButton.addEventListener('click', () => {
+  const weatherData = api.makeLocationSearch(navSearchBar.value)
+  console.log(weatherData)
+})
+
+const startupSearchBar = document.querySelector('.startup-image input')
+const startupSearchButton = document.querySelector('.startup-image button')
+startupSearchButton.addEventListener('click', () => {
+  const weatherData = api.makeLocationSearch(startupSearchBar.value)
+  console.log(weatherData)
+})
+
+const unitButton = document.querySelector('.unit-button')
+unitButton.addEventListener('click', () => {
+  const newUnits = api.changeApiUnits()
+  dom.updateUnitText(newUnits)
+})
