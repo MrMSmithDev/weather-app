bf3361494c80e2db5001d8bc800dfd53b457718f
mbright90 <michael.bright90@icloud.com>
2022-10-18 11:16:43 +0100

    Adapt gb postcode regex/Add conditionals to location data search

--- src/utilities/apiManager.js
+++ src/utilities/apiManager.js
@@ -1,11 +1,33 @@
 const apiManager = (() => {
-  let units = 'imperial'
+  let units = 'metric'
   const apiKey = '43df7ed317e5646ac516d5c73acdd3fc'
 
+  const noNumRegEx = /[0-9+]$/
+  const lonLatRegEx = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
+  const usZipRegEx = /\^[0-9]{5}(?:-[0-9]{4})?$/
+  const gbPostRegEx = /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/i
+
+  function checkSearchType(searchTerm) {
+    if (!searchTerm.match(noNumRegEx)) return 'locationName'
+    if (searchTerm.match(usZipRegEx) || searchTerm.match(gbPostRegEx)) return 'postcode'
+    if (searchTerm.match(lonLatRegEx)) return 'lonLat'
+    return 'unknown'
+  }
+
   async function getLocationData(searchTerm) {
+    let searchQuery
+    const searchType = checkSearchType(searchTerm)
+
+    if (searchType === 'LocationName' || searchType === 'unknown') searchQuery = `direct?q=${searchTerm}`
+    if (searchTerm === 'postcode') searchQuery = `zip?zip=${searchTerm}`
+    if (searchTerm === 'lonLat') {
+      const splitSearch = searchTerm.replace(/\s/, '').split(',')
+      searchQuery = `reverse?lat=${splitSearch[0]}&lon=${splitSearch[1]}`
+    }
+
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=5&appid=${apiKey}`,
+        `http://api.openweathermap.org/geo/1.0/${searchQuery}&limit=1&appid=${apiKey}`,
         {mode: 'cors'},
       )
       const responseData = await response.json()
