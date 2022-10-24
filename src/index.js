b4c274b546df7349319feadc830f8c93059b5b7d
mbright90 <michael.bright90@icloud.com>
2022-10-24 11:54:45 +0100

    Reintergrate event listeners

--- src/index.js
+++ src/index.js
@@ -2,21 +2,32 @@ import './styles/style.css'
 import './assets/fontawesome-css/css/fontawesome.css'
 import './assets/fontawesome-css/css/solid.css'
 import dom from './utilities/domManager'
+import api from './utilities/apiManager'
 
-// async function showForecast(inputBar) {
-//   dom.removeMain()
-//   dom.showLoading()
-//   try {
-//     const weatherData = await api.makeLocationSearch(inputBar.value)
-//     dom.removeMain()
-//     dom.showCurrentForecast(weatherData, api.getCurrentUnits())
-//   } catch(err) {
-//     dom.removeMain()
-//     dom.showErrorModal()
-//   }
-// }
 
 dom.showPage()
+const input = document.querySelector('input')
+
+async function showForecast() {
+  try {
+    const weatherData = await api.makeLocationSearch(input.value)
+    console.log(weatherData)
+    // dom.removeMain()
+    // dom.showCurrentForecast(weatherData, api.getCurrentUnits())
+  } catch(err) {
+    console.log(err)
+    // dom.removeMain()
+    // dom.showErrorModal()
+  }
+}
+
+const searchButton = document.querySelector('.search-container a')
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    showForecast()
+  }
+})
+searchButton.addEventListener('click', showForecast)
 
 // const searchBar = document.querySelector('#nav-location-input')
 // const navSearchButton = document.querySelector('nav .search-button')
